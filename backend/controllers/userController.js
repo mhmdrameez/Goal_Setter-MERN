207a863bf0dfba9de9ec2b641eb1655a4d4fc9d4
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2022-03-21 21:04:26 +0530

    JWT included

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     }
     else
@@ -71,7 +72,6 @@ const loginUser = asyncHandler(async(req,res)=>{
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -89,6 +89,12 @@ const getMe = asyncHandler(async(req,res)=>{
     })
 })
 
+//Generate JWT
+const generateToken = (id) =>{
+  return jwt.sign({id},process.env.JWT_SECRET,{
+    expiresIn:'30d',
+  })
+}
 
 
 module.exports = {
