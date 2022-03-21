25db29dfc9adc5ef639c4623b560335482189fa1
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2022-03-21 14:16:57 +0530

    Completed CRED functionality For the Api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,16 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // @desc Get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler( async (req,res) =>{
-    res.status(200).json({'message':'Get goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
+
+
 
 
 // @desc Set goals
@@ -16,7 +21,13 @@ const setGoals = asyncHandler( async (req,res) =>{
      res.status(400)
      throw  Error('Please Add a text field')
     }
-    res.status(200).json({'message':'Set goals'})
+
+    const goal = await Goal.create({
+        text:req.body.text,
+    })
+
+
+    res.status(200).json(goal)
 })
 
 
@@ -25,7 +36,17 @@ const setGoals = asyncHandler( async (req,res) =>{
 //@route PUT /api/goals/:id
 //@access Private
 const updateGoals =asyncHandler( async (req,res) =>{
-    res.status(200).json({'message':`Update goals ${req.params.id}`})
+const goals = await Goal.findById(req.params.id)
+if(!goals){
+    res.status(400)
+    throw new Error('Goal not found')
+}
+
+const   updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+})
+    
+    res.status(200).json(updatedGoal)
 })
 
 
@@ -33,7 +54,14 @@ const updateGoals =asyncHandler( async (req,res) =>{
 //@route DELETE /api/goals/:id
 //@access Private
 const delGoals = asyncHandler( async (req,res) =>{
-    res.status(200).json({'message':`Delete goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+}
+  await goal.remove()
+
+    res.status(200).json({id:req.params.id})
 })
 
 
