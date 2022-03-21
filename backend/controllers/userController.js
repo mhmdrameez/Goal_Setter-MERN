82ad74dd38415071bf6113349ff0219a6dd10ecd
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2022-03-21 20:49:25 +0530

    user registration and login api ready

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt =require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
+const userModel = require('../models/userModel')
 
 
 // @desc Register a new User
@@ -15,6 +16,32 @@ const registerUser = asyncHandler(async (req, res) => {
       throw new Error('Please add all fields')
     }
 
+    //Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+
+    if(user)
+    {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }
+    else
+    {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }   
+
+    
     //Check if user exists
     const userExists = await User.findOne({email})
 
@@ -31,13 +58,24 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 })
 
-// @desc Autenicatiacte a user
+// @desc  Authenticate a user
 //@route POST /api/users/login
-//@access Private
+//@access Public
 const loginUser = asyncHandler(async(req,res)=>{
+  const {email,password} = req.body
+
+  const user = await User.findOne({email})
+
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
-        message:'Login User'
+      _id: user.id,
+      name: user.name,
+      email: user.email,
     })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
 })
 
 
